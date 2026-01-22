097c9d5ecea4d32c0c175fe79b627caefe328cb6
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-22 17:19:27 +0530

    Create codes.js

---
+++ codes.js
@@ -0,0 +1,11 @@
+// ===============================
+// SUDDA ACTIVATION CODES
+// ===============================
+// FORMAT: "CODE": days
+
+const activationCodes = {
+  "SUDDA7": 7,          // 7 days
+  "SUDDA30": 30,        // 30 days
+  "SUDDA90": 90,        // 90 days
+  "SUDDALIFE": 9999     // lifetime
+};
